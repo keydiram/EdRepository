dff9d0bcdf7a3eb79c5ffa3b94c1a61a211f9608
keydiram <keydiram@yahoo.com>
2014-12-23 08:19:14 +0200

    commit 2

--- src/com/myorg/javacourse/Google_App_Engine_projectServlet.java
+++ src/com/myorg/javacourse/Google_App_Engine_projectServlet.java
@@ -7,7 +7,15 @@ import javax.servlet.http.*;
 public class Google_App_Engine_projectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		int num1, num2, num3, result;
+		num1= 4;
+		num2= 3;
+		num3= 7;
+		result = (num1+num2)*num3;
+		String str = new String("<h1> The result of ("+num1+"+"+num2+") *"+num3+ " = "+result+"</h1>");
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world Ed");
+		//resp.getWriter().println("Hello, world Ed");
+		resp.getWriter().println(str);
+		
 	}
 }
