c0ecc84bd5ead636e2ed68a9b0bbe8c3706146c8
keydiram <keydiram@yahoo.com>
2014-12-23 08:33:01 +0200

    commit 4

--- src/com/myorg/javacourse/Google_App_Engine_projectServlet.java
+++ src/com/myorg/javacourse/Google_App_Engine_projectServlet.java
@@ -13,7 +13,7 @@ public class Google_App_Engine_projectServlet extends HttpServlet {
 		num3= 7;
 		result = (num1+num2)*num3;
 		String str = new String("<h1> The result of ("+num1+"+"+num2+") *"+num3+ " = "+result+"</h1>");
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html");
 		//resp.getWriter().println("Hello, world Ed");
 		resp.getWriter().println(str);
 		
