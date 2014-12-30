5f73e285ce2c5f0a38d5209f5b8428ff931608ed
keydiram <keydiram@yahoo.com>
2014-12-30 10:14:21 +0200

    EX.3

--- src/com/myorg/javacourse/GoogleAppEngineServlet.java
+++ src/com/myorg/javacourse/GoogleAppEngineServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
-public class Google_App_Engine_projectServlet extends HttpServlet {
+public class GoogleAppEngineServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		int num1, num2, num3, result;
